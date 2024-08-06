912aac1c247979291caffcbbbd5ce26e3e6bac6f
Patricio <patriciopherrero@gmail.com>
2024-08-06 11:54:46 +0200

    work section done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div className='section' id="contact">Contact</div>;
+  return (
+    <section className="section py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <div>text</div>
+          {/* form */}
+          <form>form</form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
